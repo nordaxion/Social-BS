ed979990ae7af8ab399846fcc306646eaef2aec7
nordaxion <45746064+nordaxion@users.noreply.github.com>
2020-02-15 09:59:24 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -6,29 +6,56 @@ import os
 # Creates the GUI
 root = tk.Tk()
 
+# Title and Icon
 root.title("SocialBS")
 root.iconbitmap("images/icon.ico")
 
+# Creates the logo at the top of the GUI
+logo = ImageTk.PhotoImage(Image.open("images/logo_script_75.jpeg"))
+logo_label = tk.Label(root, image=logo)
+logo_label.pack()
+
+# Sets the canvas up
 canvas = tk.Canvas(root, height=720, width=1280, bg="#4CA3DD")
 canvas.pack()  # Attaches canvas to the root
 
-logo = ImageTk.PhotoImage(Image.open("images/logo.jpeg"))
-logo_label = tk.Label(image=logo)
-logo_label.pack()
+"""
+    This whole section is for the search interface
+"""
+# Canvas for searches
+blank_search = tk.Frame(canvas, bg="white")
+blank_search.place(relwidth=0.9, relheight=0.2, relx=0.05, rely=0.05)
 
-frame = tk.Frame(root, bg="white")
-frame.place(relwidth=0.9, relheight=0.9, relx=0.05, rely=0.05)
+# Creates a search frame
+search_frame = tk.Frame(blank_search, bg="white")
+search_frame.place(relwidth=0.4, relheight=1, relx=0.3)
 
-# Creates an input field
-usernameField = Entry(frame, width=30)
-usernameField.pack()
+# Creates an input field in the frame
+empty_search1 = tk.Label(search_frame)
+empty_search2 = tk.Label(search_frame)
+usernameLabel = tk.Label(search_frame, text="Enter username")
+usernameField = Entry(search_frame, width=30)
+usernameLabel.grid(row=2, column=0)
+usernameField.grid(row=2, column=1)
+# usernameField.pack()
 
-openFile = tk.Button(frame, text="Open File", padx=10,
-                     pady=5, fg="white", bg="red")
-openFile.pack()
+openFile = tk.Button(search_frame, text="Search", padx=10,
+                     pady=5, fg="#4CA3DD", bg="red")
+openFile.grid(row=2, column=0, columnspan=2)
 
-runApps = tk.Button(frame, text="Run Apps", padx=10,
-                    pady=5, fg="white", bg="black")
+
+"""
+    Data Section
+"""
+
+# blank_data = tk.Frame(canvas, bg="white")
+
+# blank_data.place(relwidth=0.9, relheight=0.8, relx=0.05, rely=0.15)
+
+
+
+
+
 
 # Runs the GUI
 root.mainloop()
